a8a6cb1b145024e32d988a428ad22415fca9a564
Maksym Zyza <mmziza88@gmail.com>
2023-12-12 12:54:06 +0200

    feat: task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.getElementById("ingredients");
+const list = document.createElement("li");
+
+const listItems = ingredients.map((item) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = item;
+  listItem.className = item;
+  return listItem;
+});
+
+listItems.forEach((item) => ingredientsList.append(item));
+
+[...ingredientsList.children].forEach((el) => {
+  console.log(el.outerHTML);
+});
